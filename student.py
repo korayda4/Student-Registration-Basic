156f5173e9d0b537f36521ad009f79da35138651
Koray <51831428+korayda4@users.noreply.github.com>
2023-04-26 01:13:57 +0300

    Add files via upload

--- student.py
+++ student.py
@@ -1,6 +1,20 @@
 import os
 import datetime
+import colorama
+from colorama import Fore
 ogrenciler = []
+
+#Geçici değişkene kayıt al
+def ramkaydet():
+        kayittarihi = datetime.datetime.now().strftime(format='%m/%d/%y') + f" Saat: {str(datetime.datetime.now().hour)}:{str(datetime.datetime.now().minute)}"
+
+
+    #ogrenci listesinde diziye isim ver ve her isime değişken ata
+        ogrenci = {"ad": ad.upper(), "soyad": soyad.upper(), "yas": yas, "bölüm": bölüm.upper(), "numara": numara,"cinsiyet":cinsiyet.upper(),"tarih":kayittarihi}  
+
+        #ogrencideki bilgileri tek tek ogrenciler listesine kayıt et
+        ogrenciler.append(ogrenci)
+        kaydet(ogrenciler) 
 
 #TXT dosyasına ram'de tutulan bilgilerin kayıdı için fonksiyon
 def kaydet(ogrenciler):
@@ -20,124 +34,134 @@ def yukle():
                 ogrenciler.append(ogrenci)
     return ogrenciler
 
+#Tekrar işlem yapmak istiyor mu diye sor
+def tekrar(): 
+    devam = input(Fore.LIGHTGREEN_EX+"Tekrar İşlem yapmak istiyor musunuz?(E/H):\n-->")                         
 
+        #Büyük E küçük e ayrımını düzelt ve e ye eşit değilse sonlandır eşitse devam et
+    if devam.lower() == "e":                        
+            return(True)
+    elif devam.lower() == "h":
+            print(Fore.RED+"Görüşmek üzere...")
+            kaydet(ogrenciler)
+            return(False)                  
+    
+#Okutulacak değeri iste ve okut    
+def okutucu(deger,aranacakdeger):
+    if str(deger) in ogrenci[aranacakdeger]:
+        print(Fore.MAGENTA+"\nAdı: {}  \nSoyadı: {} \nYaşı: {} \nBölümü: {} \nOkul Numarası: {}\nCinsiyeti: {}\nKayıt Tarihi: {}\n".format(ogrenci["ad"], ogrenci["soyad"], ogrenci["yas"], ogrenci["bölüm"], ogrenci["numara"],ogrenci["cinsiyet"],ogrenci["tarih"]))
+    else:
+        print(Fore.RED+"Öğrenci bulunamadı veya kayıtlı değil")
 
+#Değiştirilecek bilgiyi iste ve değiştir
+def bilgiyenile(deger):                   
+            ogrenci[deger] = input(Fore.YELLOW+"Yeni bilgiyi giriniz:\n-->").upper()
+            while len(ogrenci[deger])<2 :
+                  print(Fore.RED+f"Lütfen doğru giriniz")
+                  ogrenci[deger] = input(Fore.YELLOW+"Yeni Bilgi:\n--> ").upper()
+                  if deger != "ad" or deger != "soyad":
+                      if not ogrenci[deger].isalpha():
+                          print(Fore.RED+f"Lütfen doğru giriniz")
+                          ogrenci[deger] = input(Fore.YELLOW+"Yeni Bilgi:\n--> ").upper()
+            kaydet(ogrenciler)              
+                      
+            print(Fore.GREEN+"Başarıyla değiştirildi")
+            devam = input(Fore.GREEN+"Tekrar İşlem yapmak istiyor musunuz?(E/H):\n-->")                         
+
+        #Büyük E küçük e ayrımını düzelt ve e ye eşit değilse sonlandır eşitse devam et
+            if devam.lower() == "e":                        
+               return(True)
+            elif devam.lower() == "h":
+              print("Görüşmek üzere...")
+              kaydet(ogrenciler)
+              return(False)
 
 kayittarihi = ""
+
+#TXT dosyasındaki kayıtları geçici değişkene ver
 ogrenciler = yukle()
+
+#Önceden bölümleri belirle
 bölümler = ["YAZILIM","HUKUK","MUHASEBE","MÜZIK","MATBAA","ÖGRETMENLIK","PSIKOLOGLUK","BILGISAYAR MÜHENDISLIGI"]
 while True:
+   
     #İşlem türünü seçtir
-    islem = input("\nİşlem türü seçiniz:\n1)Öğrenci Kayıt\n2)Öğrenciler\n3)öğrenci Sorgu\n4)öğrenci bilgi yenile\n5)bölüm öğrencilerini listele\n6)Cinsiyete göre listele\n-->")   
+    islem = input(Fore.BLUE+"\nİşlem türü seçiniz:\n1)Öğrenci Kayıt\n2)Öğrenciler\n3)öğrenci Sorgu\n4)öğrenci bilgi yenile\n5)bölüm öğrencilerini listele\n6)Cinsiyete göre listele\n-->")   
     
+    #Eğer işlem cinsiyete göre listele ise çalıştır
     if islem.lower() == "cinsiyete göre listele" or islem.lower() == "6":
         cinsiyet = input("Listelenecek cinsiyeti giriniz:\n-->")
+        aranacakdeger = "cinsiyet"
         for ogrenci in ogrenciler:
-            if cinsiyet.upper() in ogrenci["cinsiyet"]:
-                print("\nAdı: {}  \nSoyadı: {} \nYaşı: {} \nBölümü: {} \nOkul Numarası: {}\nCinsiyeti: {}\nKayıt Tarihi: {}\n".format(ogrenci["ad"], ogrenci["soyad"], ogrenci["yas"], ogrenci["bölüm"], ogrenci["numara"],ogrenci["cinsiyet"],ogrenci["tarih"]))
+            okutucu(cinsiyet.upper(),aranacakdeger)
 
-        devam = input("Tekrar İşlem yapmak istiyor musunuz?(E/H):\n-->")                         
-
-        #Büyük E küçük e ayrımını düzelt ve e ye eşit değilse sonlandır eşitse devam et
-        if devam.lower() == "e":                        
+        if tekrar() == True:
             continue
-        elif devam.lower() == "h":
-            print("Görüşmek üzere...")
-            kaydet(ogrenciler)
+        else:
             break
 
-
-
-
+    #Eğer işlem cinsiyete göre listele ise çalıştır
     if islem.lower() == "bölüm öğrencilerini listele" or islem.lower() == "5":
         bölüm = input("Listelenecek bölümü giriniz:\n-->")
+        aranacakdeger = "bölüm"
         for ogrenci in ogrenciler:
-            if bölüm.upper() in ogrenci["bölüm"]:
-                print("\nAdı: {}  \nSoyadı: {} \nYaşı: {} \nBölümü: {} \nOkul Numarası: {}\nCinsiyeti: {}\nKayıt Tarihi: {}\n".format(ogrenci["ad"], ogrenci["soyad"], ogrenci["yas"], ogrenci["bölüm"], ogrenci["numara"],ogrenci["cinsiyet"],ogrenci["tarih"]))
+            okutucu(bölüm.upper(),aranacakdeger)
 
-        devam = input("Tekrar İşlem yapmak istiyor musunuz?(E/H):\n-->")                         
-
-        #Büyük E küçük e ayrımını düzelt ve e ye eşit değilse sonlandır eşitse devam et
-        if devam.lower() == "e":                        
+        if tekrar() == True:
             continue
-        elif devam.lower() == "h":
-            print("Görüşmek üzere...")
-            kaydet(ogrenciler)
-            break        
+        else:
+            break
 
-    #Eğer işlem öğrenci kayıt ise değişkenleri girmesini iste
+    #Eğer işlem öğrenci kayıt ise çalıştır
     if islem.lower() == "öğrenci kayıt" or islem.lower() == "1":  
 
         #İstediğimiz Değişkenler ve değişken türü koruması
         #While not kodu ile istediğimiz değişkeni veya girişi zorunlu kılıyoruz
-        ad = input("\nİsim:\n--> ")
-        while len(ad)<=2:
-            print("Lütfen isim giriniz")
-            ad = input("İsim:\n--> ")
-        while not ad.isalpha():
-            print("Lütfen isim giriniz")
-            ad = input("İsim:\n--> ")
+        ad = input(Fore.YELLOW+"\nİsim:\n--> ")
+        while len(ad)<=2 or not ad.isalpha():
+            print(Fore.RED+"Lütfen isim giriniz")
+            ad = input(Fore.YELLOW+"İsim:\n--> ")
+        
+        soyad = input(Fore.YELLOW+"Soyisim:\n--> ")
+        while len(ad)<=1 or soyad.isdigit():
+            print(Fore.RED+"Lütfen soyisim giriniz")
+            soyad = input(Fore.YELLOW+"Soyisim:\n--> ")
+        
 
-        soyad = input("Soyisim:\n--> ")
-        while len(ad)<=1:
-            print("Lütfen soyisim giriniz")
-            soyad = input("Soyisim:\n--> ")
-        while not soyad.isalpha():
-            print("Lütfen soyisim giriniz")
-            soyad = input("Soyisim:\n--> ")
-
-        yas = input("Yaş:\n--> ")
-        while not yas.isdigit():
-            print("Lütfen Sayı giriniz")
-            yas = input("Yaş:\n--> ")
+        yas = input(Fore.YELLOW+"Yaş:\n--> ")
+        while not yas.isnumeric():
+            print(Fore.RED+"Lütfen Sayı giriniz")
+            yas = input(Fore.YELLOW+"Yaş:\n--> ")
         while int(yas)<=17:
-            print("Yaş en az 18 olabilir")
-            yas = input("Yaş:\n--> ") 
+            print(Fore.RED+"Yaş en az 18 olabilir")
+            yas = input(Fore.YELLOW+"Yaş:\n--> ") 
 
-        bölüm = input("Bölüm:\n--> ").upper()
-        while bölüm.upper() not in bölümler:
-             print("Lütfen doğru bölüm girin")
-             bölüm = input("Bölüm:\n--> ").upper()
+        bölüm = input(Fore.YELLOW+"Bölüm:\n--> ").upper()
+        while bölüm.upper() not in bölümler or not bölüm.isalpha():
+             print(Fore.RED+"Lütfen doğru bölüm girin")
+             bölüm = input(Fore.YELLOW+"Bölüm:\n--> ").upper()
 
-        while not bölüm.isalpha():
-            print("Lütfen bölüm giriniz")
-            bölüm = input("Bölüm:\n--> ")
-
-        numara = input("Okul Numarası:\n--> ")
+        numara = input(Fore.YELLOW+"Okul Numarası:\n--> ")
         
         for j in ogrenciler:
                   if numara in j["numara"]:
-                    print("Bu numara başka öğrenciye atanmış")
-                    numara = input("Okul Numarası:\n--> ")
+                    print(Fore.RED+"Bu numara başka öğrenciye atanmış")
+                    numara = input(Fore.YELLOW+"Okul Numarası:\n--> ")
                   
         while not numara.isdigit():
-            print("Lütfen Sayı giriniz")
-            numara = input("Okul Numarası:\n--> ")
+            print(Fore.RED+"Lütfen Sayı giriniz")
+            numara = input(Fore.YELLOW+"Okul Numarası:\n--> ")
 
-        cinsiyet = input("Cinsiyeti:\n--> ")
+        cinsiyet = input(Fore.YELLOW+"Cinsiyeti:\n--> ")
         while not (cinsiyet.lower()=="erkek" or cinsiyet.lower()=="kadın"):
-            print("Lütfen Cinsiyeti Erkek veya Kadın olarak belirleyin")
-            cinsiyet = input("Cinsiyeti:\n--> ")
+            print(Fore.RED+"Lütfen Cinsiyeti Erkek veya Kadın olarak belirleyin")
+            cinsiyet = input(Fore.YELLOW+"Cinsiyeti:\n--> ")
 
-            #Tarihi ve saati kaydet
-        kayittarihi = datetime.datetime.now().strftime(format='%m/%d/%y') + f" Saat: {str(datetime.datetime.now().hour)}:{str(datetime.datetime.now().minute)}"
+        ramkaydet()
 
-        #ogrenci listesinde diziye isim ver ve her isime değişken ata
-        ogrenci = {"ad": ad.upper(), "soyad": soyad.upper(), "yas": yas, "bölüm": bölüm.upper(), "numara": numara,"cinsiyet":cinsiyet.upper(),"tarih":kayittarihi}  
-
-        #ogrencideki bilgileri tek tek ogrenciler listesine kayıt et
-        ogrenciler.append(ogrenci)
-        kaydet(ogrenciler) 
-
-        #Devam etmek istiyormu diye sor
-        devam = input("Tekrar İşlem yapmak istiyor musunuz?(E/H):\n-->")                         
-
-        #Büyük E küçük e ayrımını düzelt ve e ye eşit değilse sonlandır eşitse devam et
-        if devam.lower() == "e":                        
+        if tekrar() == True:
             continue
-        elif devam.lower() == "h":
-            print("Görüşmek üzere...")
-            kaydet(ogrenciler)
+        else:
             break
 
     #Eğer işlem öğrencilerse çalıştır
@@ -147,26 +171,23 @@ while True:
             for ogrenci in ogrenciler: 
 
                 #ogrenciler listesindeki her değer için ogrenciyi değiştir ve str atanan değişkenleri formatla yazdır
-                print("\nAdı: {} \nSoyadı: {} \nYaşı: {} \nBölümü: {} \nOkul Numarası: {}\nCinsiyeti: {}\nKayıt Tarihi: {}\n".format(ogrenci["ad"], ogrenci["soyad"], ogrenci["yas"], ogrenci["bölüm"], ogrenci["numara"],ogrenci["cinsiyet"],ogrenci["tarih"]))
+                print(Fore.MAGENTA+"\nAdı: {} \nSoyadı: {} \nYaşı: {} \nBölümü: {} \nOkul Numarası: {}\nCinsiyeti: {}\nKayıt Tarihi: {}\n".format(ogrenci["ad"], ogrenci["soyad"], ogrenci["yas"], ogrenci["bölüm"], ogrenci["numara"],ogrenci["cinsiyet"],ogrenci["tarih"]))
  
         #ogrenci karakter uzunluğu 0 ise öğrenci kayıdı yoktur de
         elif len(ogrenciler) == 0:
-            print("Kayıtlı Öğrenci Yok")       
+            print(Fore.RED+"Kayıtlı Öğrenci Yok")       
         
-        #Devam etmek istiyormu diye sor
-        devam = input("Tekrar İşlem yapmak istiyor musunuz?(E/H):\n-->")    
-
-        #Büyük E küçük e ayrımını düzelt ve e ye eşit değilse sonlandır eşitse devam et
-        if devam.lower() != "e":                   
-              print("Görüşmek Üzere...")
-              kaydet(ogrenciler)
-              break
+        if tekrar() == True:
+            continue
+        else:
+            break
         
     #eğer işlem öğrenci sorguysa çalıştır
     elif islem.lower() == "öğrenci sorgu" or islem.lower() == "3":   
 
         #aranan öğrencinin numarasını iste
-        numara = input("Öğrencinin numarasını giriniz:\n--> ")  
+        ara = input("Öğrencinin numarasını veya Adını giriniz:\n--> ") 
+        aranacakdeger = "numara" 
 
         #ogrenciker karakter uzunluğu 0 ise öğrenci kayıdı yoktur de
         if len(ogrenciler) == 0:
@@ -175,20 +196,14 @@ while True:
         #ogrenciler listesindeki her değer için ogrenciyi değiştir
         elif len(ogrenciler) != 0:   
             for ogrenci in ogrenciler:
-                if numara in ogrenci["numara"]:                          
-                    print("\nAdı: {}  \nSoyadı: {} \nYaşı: {} \nBölümü: {} \nOkul Numarası: {}\nCinsiyeti: {}\nKayıt Tarihi: {}\n".format(ogrenci["ad"], ogrenci["soyad"], ogrenci["yas"], ogrenci["bölüm"], ogrenci["numara"],ogrenci["cinsiyet"],ogrenci["tarih"]))
-              
-        else:
-            print("Öğrenci Kayıtlı Değil!")
+                okutucu(ara.upper(),aranacakdeger)
+                aranacakdeger = "ad"
+                okutucu(ara.upper(),aranacakdeger)
                        
-        #Devam etmek istiyormu diye sor
-        devam = input("Tekrar İşlem yapmak istiyor musunuz?(E/H):\n-->")    
-
-        #Büyük E küçük e ayrımını düzelt ve e ye eşit değilse sonlandır eşitse devam et
-        if devam.lower() != "e":                        
-               print("Görüşmek Üzere...")
-               kaydet(ogrenciler)
-               break
+        if tekrar() == True:
+            continue
+        else:
+            break
    
     #işlem Öğrenci bilgi yenile ise çalıştır
     elif islem.lower() == "öğrenci bilgi yenile" or islem.lower() == "4" :
@@ -204,34 +219,25 @@ while True:
                     
                     #değiştirlimek istenen bilgiyi sor ve girilen bilgiye göre değişkeni değiştir
                     if değiştir == "isim":                    
-                       ogrenci["ad"] = input("Yeni bilgiyi giriniz:\n-->").upper()
-                       while len(ogrenci["ad"])<2:
-                         print("Lütfen isim giriniz")
-                         ogrenci["ad"] = input("İsim:\n--> ").upper()
-                       while not ogrenci["ad"].isalpha():
-                        print("Lütfen isim giriniz")
-                        ogrenci["ad"] = input("İsim:\n--> ").upper()
-                       print("Başarıyla değiştirildi")
+                       bilgiyenile("ad")
+                       if bilgiyenile == True:
+                         continue
+                       else:
+                         break
 
                     elif değiştir == "soyisim":
-                       ogrenci["soyad"] = input("Yeni bilgiyi giriniz:\n-->").upper()
-                       while len(ogrenci["soyad"])<=2:
-                         print("Lütfen soyisim giriniz")
-                         ogrenci["soyad"] = input("Soyisim:\n--> ").upper()
-                       while not ogrenci["soyad"].isalpha():
-                        print("Lütfen soyisim giriniz")
-                        ogrenci["soyad"] = input("Soyisim:\n--> ").upper()
-                       print("Başarıyla değiştirildi")
+                       bilgiyenile("soyad")
+                       if bilgiyenile == True:
+                         continue
+                       else:
+                         break
 
                     elif değiştir == "yaş":
-                        ogrenci["yas"] = input("Yeni bilgiyi giriniz:\n-->").upper()
-                        while int(ogrenci["yas"])<=2:
-                         print("Lütfen Yaş giriniz")
-                         ogrenci["yas"] = input("Yaş:\n--> ").upper()
-                        while not ogrenci["yas"].isdigit():
-                         print("Lütfen yaş giriniz")
-                         ogrenci["yas"] = input("Yaş:\n--> ").upper()
-                        print("Başarıyla değiştirildi")
+                        bilgiyenile("yas")
+                        if bilgiyenile == True:
+                         continue
+                        else:
+                          break
                     
                     elif değiştir == "bölüm":
                         ogrenci["bölüm"] = input("Yeni bilgiyi giriniz:\n-->").upper()
@@ -241,14 +247,7 @@ while True:
                         print("Başarıyla değiştirildi")
                         
                     elif değiştir == "okul numarası":
-                        ogrenci["numara"] = input("Yeni bilgiyi giriniz:\n-->").upper()
-                        while ogrenci["numara"]<=2:
-                         print("Lütfen Numara giriniz")
-                         ogrenci["numara"] = input("Numara:\n--> ").upper()
-                        while not ogrenci["numara"].isdigit():
-                         print("Lütfen yaş giriniz")
-                         ogrenci["numara"] = input("Numara:\n--> ").upper()
-                        print("Başarıyla değiştirildi")
+                        bilgiyenile("numara")
 
                     elif değiştir == "cinsiyet":
                         ogrenci["cinsiyet"] = input("Yeni bilgiyi giriniz:\n-->").upper()
@@ -256,21 +255,10 @@ while True:
                             print("Lütfen Cinsiyeti Erkek veya Kadın olarak belirleyin")
                             ogrenci["cinsiyet"] = input("Cinsiyeti:\n--> ").upper()
                     else:
-                        print("Yanlış işlem girildi")
-
-
-        devam = input("Tekrar İşlem yapmak istiyor musunuz?(E/H):\n-->")    
-
-        #Büyük E küçük e ayrımını düzelt ve e ye eşit değilse sonlandır eşitse devam et
-        if devam.lower() != "e":                        
-               print("Görüşmek Üzere...")
-               kaydet(ogrenciler)
-               break            
-    
+                        print("Yanlış işlem girildi")            
                     
         else:
             print("Öğrenci Kayıtlı Değil!")
-
 
     else:
         print("Yanlış İşlem girdiniz lütfen dikkat edin!")    
